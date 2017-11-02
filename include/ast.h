d4bd3d4aedbd8fd833724d86df3342b7e3384fcd
betaest <route.addr@qq.com>
2017-11-02 10:29:20 +0800

    更新 Ast类模型定型，增加测试案例

--- include/ast.h
+++ include/ast.h
@@ -10,65 +10,72 @@
 namespace ts {
 class Ast;
 class AstNode;
-using p_ast_t = std::shared_ptr<Ast>;
 using nodes_t = std::vector<AstNode>;
 using extend_t = std::unordered_map<std::string, std::string>;
 using p_ext_t = std::shared_ptr<extend_t>;
-class AstNode {
-    friend class Ast;
+using p_ast_t = std::shared_ptr<Ast>;
+class Ast : protected nodes_t, public std::enable_shared_from_this<Ast> {
+   public:
+    using nodes_t::empty;
+    using nodes_t::size;
+    using nodes_t::begin;
+    using nodes_t::end;
+    using nodes_t::rbegin;
+    using nodes_t::rend;
+    using nodes_t::at;
+    using nodes_t::operator[];
+    using nodes_t::clear;
+    using nodes_t::iterator;
+    using nodes_t::const_iterator;
 
+    AstNode &add(std::string tag, std::string text = "");
+    AstNode &add(std::string tag, extend_t _init);
+
+    p_ast_t remove(const AstNode &node);
+    p_ast_t remove(size_t pos);
+    // void clear() { nodes_.clear(); }
+
+   public:
+    explicit Ast() {}
+};
+
+class AstNode {
    private:
     std::string tag_;
-    std::string text_;
+    // std::string text_;
     p_ext_t extends_;
     p_ast_t children_;
-    nodes_t::iterator me_;
 
    public:
     const std::string tag() const { return tag_; }
-    const std::string text() const { return text_; }
+    // const std::string text() const { return text_; }
 
-    void set_tag(std::string tag) { tag_ = tag; }
-    void set_text(std::string text) { text_ = text; }
+    void tag(std::string tag) { tag_ = tag; }
+    // void text(std::string text) { text_ = text; }
 
     p_ext_t extends() { return extends_; }
+    const std::string &extends(std::string key) const {
+        return extends_->at(key);
+    }
     extend_t &extends_r() { return *extends_; }
     p_ast_t children() { return children_; }
     Ast &children_r() { return *children_; }
-
-    const nodes_t::iterator me() const { return me_; }
-
-   public:
-    const nodes_t::const_iterator begin();
-    const nodes_t::const_iterator end();
+    const AstNode &children(size_t pos) const { return children_->at(pos); }
 
    public:
-    AstNode(std::string _tag, std::string _text = "", p_ast_t _parent = nullptr)
-        : tag_(_tag), text_(_text) {
-        children_ = std::make_shared<Ast>(_parent);
+    const nodes_t::const_iterator begin() { return children_->begin(); }
+    const nodes_t::const_iterator end() { return children_->end(); }
+    bool operator==(const AstNode &node) { return this == &node; }
+
+   public:
+    explicit AstNode(std::string _tag, std::string _text = "") : tag_(_tag) {
+        children_ = std::make_shared<Ast>();
         extends_ = std::make_shared<extend_t>();
+        if (!_text.empty()) extends_->emplace("value", _text);
     }
-};
 
-class Ast : public std::enable_shared_from_this<Ast> {
-   private:
-    nodes_t nodes_;
-    p_ast_t parent_;
-
-   public:
-    const bool empty() const { return nodes_.empty(); }
-    const size_t size() const { return nodes_.size(); }
-
-    const AstNode &at(int index) const { return nodes_.at(index); }
-    const nodes_t::const_iterator begin() const { return nodes_.begin(); }
-    const nodes_t::const_iterator end() const { return nodes_.end(); }
-
-    AstNode &add(std::string tag, std::string text = "");
-    p_ast_t remove(AstNode node);
-    p_ast_t remove(int pos);
-    void clear() { nodes_.clear(); }
-
-   public:
-    explicit Ast(p_ast_t _parent = nullptr) : parent_(_parent) {}
+    explicit AstNode(std::string _tag, extend_t _init) : AstNode(_tag) {
+        extends_->swap(_init);
+    }
 };
 }
