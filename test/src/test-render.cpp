a04a739f33dd9d565319cc65269fe97e7e90ae5f
robotcat2008 <304wzq@163.com>
2017-11-15 21:12:25 +0800

    编写测试案例

--- test/src/test-render.cpp
+++ test/src/test-render.cpp
@@ -4,7 +4,10 @@
 #include <iostream>
 #include <sstream>
 
+#include <tools.h>
+
 using namespace std;
+using namespace html;
 
 TEST_CASE("basic render", "[render]") {
     gfm::Document document;
@@ -16,4 +19,13 @@ TEST_CASE("basic render", "[render]") {
     auto h = render.render(p);
 
     cout << h << endl;
+}
+
+TEST_CASE("encodeString test", "[render]") {
+    //std::string str{"< > &amp; & &#x10; \""};
+    std::string str{"< > & &amp; &amp; \""};
+
+    //REQUIRE(encodeString(str, cAmps|cDoubleAmps|cAngles|cQuotes) =="&lt; &gt; &amp; &amp; &#x10; &quot;") ;
+    REQUIRE(encodeString(str, cAmps|cDoubleAmps|cAngles|cQuotes) =="&lt; &gt; &amp; &amp; &amp;  &quot;") ;
+
 }
